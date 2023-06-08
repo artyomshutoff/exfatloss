98c77de7d16c0834fca1f5a1fca16a8fa3136746
Artyom Shutoff <37169190+artyomshutoff@users.noreply.github.com>
2023-06-08 21:41:00 +0300

    Add files via upload

---
+++ chart.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+files = ['1. Keto.xlsx', 
+         '2. Carnivore.xlsx', 
+         '3. Lion Diet.xlsx', 
+         '4. Raw Lion Diet.xlsx', 
+         '5. Lion Diet.xlsx',
+         '6. Карнивор.xlsx',
+         ]
+
+weight = []
+time = []
+
+for f in files:
+
+    a = pd.read_excel(f)
+    
+    x = list(a['Дата'].dropna())
+    y = list(a['Вес'].dropna())
+    
+    ms = min(len(x), len(y))
+    
+    x = x[:ms]
+    y = y[:ms]
+    
+    time += x
+    weight += y
+
+time2 = [time[i].timestamp() for i in range(len(time))]
+
+fig, ax = plt.subplots()
+fig.canvas.manager.set_window_title('Ex Fat Loss')
+
+model = np.poly1d(np.polyfit(time2, weight, 3))
+deriv = model.deriv()
+
+ax.plot(time, model(time2), color='#8acfa0', alpha=0.5)
+ax.axhspan(60, 81, facecolor='#82be48', alpha=0.25)
+ax.axhspan(81, 97, facecolor='#ffc232', alpha=0.25)
+ax.axhline(75, color='r', alpha=0.5)
+ax.scatter(time, weight, facecolors='none', edgecolors='#1f77b4', s=10)
+
+fig.show()
